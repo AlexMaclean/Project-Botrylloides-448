435895b3d277dd66049ed38f2559f9568b51c984
Alex MacLean <alex.maclean2000@gmail.com>
2020-11-09 08:35:37 -0800

    cleanup

--- ecoli/jf-hash-experiment/graphs.R
+++ ecoli/jf-hash-experiment/graphs.R
@@ -1,9 +1,17 @@
 source("../../scripts/graphs.R")
 
-data <- read.csv(file = 'data-11-8.csv', header = TRUE)
-x <- data$k
-xlab <- "K-mer Size"
+data <- read.csv(file = 'data-11-9.csv', header = TRUE)
+x <- data$jf.size
+xlab <- "Jelly Fish Hash Size"
+
+recomended.line <- function() {
+  recomended.size <- 5595799 * 20
+  abline(v = recomended.size, col = "grey", lty = 2)
+}
 
 plot.length.n50(data, x, xlab)
+recomended.line()
 plot.num.contigs(data, x, xlab)
+recomended.line()
 plot.runtimes(data, x, xlab, "topleft", c(0.1, 0.2))
+recomended.line()
