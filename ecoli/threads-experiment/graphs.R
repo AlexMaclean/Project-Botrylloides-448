7a8001455be715b215f91a99f0adae5d4f842477
Alex MacLean <alex.maclean2000@gmail.com>
2020-11-08 21:45:26 -0800

    threads ggraph

--- ecoli/threads-experiment/graphs.R
+++ ecoli/threads-experiment/graphs.R
@@ -1,9 +1,7 @@
 source("../../scripts/graphs.R")
 
-data <- read.csv(file = 'data-11-8.csv', header = TRUE)
+data <- read.csv(file = 'data-11-8-2.csv', header = TRUE)
 x <- data$threads
 xlab <- "Thread Count"
 
-plot.length.n50(data, x, xlab)
-plot.num.contigs(data, x, xlab)
 plot.runtimes(data, x, xlab, "topright", 0.05)
