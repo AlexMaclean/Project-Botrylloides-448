a6dbb0f298437063732867f02f4a920ade5b4584
Alex MacLean <alex.maclean2000@gmail.com>
2020-11-13 17:29:24 -0800

    cleanup

--- bacteria-experiments/kmer-size/graphs.R
+++ bacteria-experiments/kmer-size/graphs.R
@@ -1,9 +1,13 @@
 source("../../scripts/graphs.R")
 
-data <- read.csv(file = 'data-11-8.csv', header = TRUE)
-x <- data$k
-xlab <- "K-mer Size"
+kmer.graphs <- function(name, data) {
+  x <- data$k
+  xlab <- "K-mer Size"
 
-plot.length.n50(data, x, xlab)
-plot.num.contigs(data, x, xlab)
-plot.runtimes(data, x, xlab, "topleft", c(0.1, 0.2))
+  plot.length.n50(name, data, x, xlab)
+  plot.num.contigs(name, data, x, xlab)
+  plot.runtimes(name, data, x, xlab, "topleft", c(0.1, 0.2))
+}
+
+kmer.graphs("E.coli", read.csv(file = 'ecoli-data-11-8.csv', header = TRUE))
+kmer.graphs("Listeria", read.csv(file = 'listeria-data-11-13.csv', header = TRUE))
--- bacteria-experiments/read-count/graphs.R
+++ bacteria-experiments/read-count/graphs.R
@@ -1,9 +1,10 @@
 source("../../scripts/graphs.R")
 
+name <- "E.coli"
 data <- read.csv(file = 'data-11-12.csv', header = TRUE)
 x <- data$Long.Read.Count
 xlab <- "Long Read Count"
 
-plot.length.n50(data, x, xlab)
-plot.num.contigs(data, x, xlab)
-plot.runtimes(data, x, xlab, "topright", c(0.1, 0.2))
+plot.length.n50(name, data, x, xlab)
+plot.num.contigs(name, data, x, xlab)
+plot.runtimes(name, data, x, xlab, "topright", c(0.1, 0.2))
